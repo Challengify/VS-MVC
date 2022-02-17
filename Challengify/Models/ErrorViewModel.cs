cb9d82c974c0e9f692f0c90bf24fd884671fc0bb
David <davidnatro03@gmail.com>
2022-02-17 14:41:16 +0300

    uploaded

---
+++ Challengify/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Challengify.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

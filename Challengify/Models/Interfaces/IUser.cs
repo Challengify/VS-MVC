aba73648de910f37ea41e2fbd25792ff390cd0c1
David <davidnatro03@gmail.com>
2022-02-22 13:42:34 +0300

    Added interface

---
+++ Challengify/Models/Interfaces/IUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Challengify.Models.Interfaces
+{
+    public interface IUser
+    {
+        IEnumerable<User> GetUsersInChallenge(Challenge challenge);
+    }
+}
